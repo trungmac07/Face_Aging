0bdd75900f9aab08cee46399f5733e3493b099f6
trungmac07 <mttrung16@gmail.com>
2024-04-05 15:01:52 +0700

    Cuda training

--- Discriminator.py
+++ Discriminator.py
@@ -4,15 +4,17 @@ import torch.nn.functional as F
 import numpy as np
 
 class Discriminator(nn.Module):
-    def __init__(self, img_size = 128, conv_dim = 64, c_dim = 5):
+    def __init__(self, img_size = 128, conv_dim = 64, c_dim = 5, device='cuda' if torch.cuda.is_available() else 'cpu'):
         super().__init__()
+        self.device = device
+
         layers = []
-        layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
+        layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1,device=self.device))
         layers.append(nn.LeakyReLU(0.01))
 
         curr_dim = conv_dim
         for i in range(1, 6):
-            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1))
+            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1,device=self.device))
             layers.append(nn.LeakyReLU(0.01))
             curr_dim = curr_dim * 2
 
@@ -20,13 +22,13 @@ class Discriminator(nn.Module):
         self.layers = nn.Sequential(*layers)
 
         self.conv1 = nn.Sequential()
-        self.conv1.append(nn.Conv2d(curr_dim, 1, kernel_size=3, stride=2, padding=1, bias=False))
+        self.conv1.append(nn.Conv2d(curr_dim, 1, kernel_size=3, stride=2, padding=1, bias=False, device=self.device))
         self.conv1.append(nn.Sigmoid())
 
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False,device=self.device)
 
     def forward(self, x):
-        x = x.float()  # Cast input tensor to float32
+        x = x.float().to(self.device)  # Cast input tensor to float32
 
         res = self.layers(x)
 
--- Model.py
+++ Model.py
@@ -8,35 +8,35 @@ from Discriminator import *
 from ReadImage import *
 
 class StarGAN(nn.Module):
-    def __init__(self, l_cls=1, l_rec=10, n_critic=1):
+    def __init__(self, l_cls=1, l_rec=10, n_critic=1, device='cuda' if torch.cuda.is_available() else 'cpu'):
         super().__init__()
         self.G = Generator()
         self.D = Discriminator()
         self.l_cls = l_cls
         self.l_rec = l_rec
         self.n_critic = n_critic
+        self.device = device
 
     def discriminator_loss(self, real_res, predicted_res, real_labels, predicted_labels):
-        real_res = real_res.float()
-        predicted_res = predicted_res.float()
-        real_labels = real_labels.float()
-        predicted_labels = predicted_labels.float()
-        print(real_res.shape, predicted_res.shape, real_labels.shape, predicted_labels.shape)
+        real_res = real_res.float().to(self.device)
+        predicted_res = predicted_res.float().to(self.device)
+        real_labels = real_labels.float().to(self.device)
+        predicted_labels = predicted_labels.float().to(self.device)
         return self.l_cls * F.cross_entropy(predicted_labels, real_labels) - F.binary_cross_entropy(predicted_res, real_res)
 
     def generator_loss(self, real_res, predicted_res, real_labels, predicted_labels, x, x_rec):
-        real_res = real_res.float()
-        predicted_res = predicted_res.float()
-        real_labels = real_labels.float()
-        predicted_labels = predicted_labels.float()
-        x = x.float()
-        x_rec = x_rec.float()
+        real_res = real_res.float().to(self.device)
+        predicted_res = predicted_res.float().to(self.device)
+        real_labels = real_labels.float().to(self.device)
+        predicted_labels = predicted_labels.float().to(self.device)
+        x = x.float().to(self.device)
+        x_rec = x_rec.float().to(self.device)
         
         return (self.l_cls * F.cross_entropy(predicted_labels, real_labels) +
                 self.l_rec * torch.mean(torch.abs(x - x_rec)) +
                 F.binary_cross_entropy(predicted_res, real_res))
 
-    def train(self, x_train, labels, n_epochs=10, batch_size=128, device='cuda' if torch.cuda.is_available() else 'cpu'):
+    def train(self, x_train, labels, n_epochs=10, batch_size=32):
 
         x_data_loader = torch.utils.data.DataLoader(x_train, batch_size=batch_size)
         labels_data_loader = torch.utils.data.DataLoader(labels, batch_size=batch_size)
@@ -45,41 +45,56 @@ class StarGAN(nn.Module):
         g_optimizer = torch.optim.Adam(self.G.parameters())
 
         for epoch in range(n_epochs):
+
+            i = 1
             for x_data, labels_data in zip(x_data_loader, labels_data_loader):
-                x = x_data.to(device)
-                x = x.permute(0, 3, 1, 2)
-                l = labels_data.to(device)
+                
+                print(f"Training on batch {i}")
+                i+=1
+                
+                x = x_data.to(self.device)
+                l = labels_data.to(self.device)
                 
                 d_optimizer.zero_grad()
 
                 # Discriminator training
-                random_labels = torch.randint(0, 5, (batch_size,), device=device)
-                random_l = F.one_hot(random_labels, num_classes=5)
-
+                print(f"Training Discriminator Process:")
+                random_labels = torch.randint(0, 5, (batch_size,), device=self.device)
+                random_l = F.one_hot(random_labels, num_classes=5).to(self.device)
+                print(f"\tGenerating Fake Image")
                 fake_img = self.G(x, random_l)
 
                 d_train_data = torch.cat((fake_img, x), dim= 0)
-                print(d_train_data.shape)
 
-                real_res = torch.cat((torch.zeros(batch_size), torch.ones(batch_size))).to(device)
+                real_res = torch.cat((torch.zeros(batch_size), torch.ones(batch_size))).to(self.device)
                 label_res = torch.cat([random_l, l], dim= 0)
 
+                print(f"\tForwarding Discriminator")
                 d_res, d_label = self.D(d_train_data)
 
                 d_loss = self.discriminator_loss(real_res, d_res, label_res, d_label)
+                print(f"\tD_loss:{d_loss}")
                 d_loss.backward()
                 d_optimizer.step()
 
                 # Generator training
                 if epoch % self.n_critic == 0:
+                    print("Training Generator")
                     g_optimizer.zero_grad()
+
+                    print("\tGenerating Fake Image")
                     x_fake = self.G(x, random_l)
+
+                    print("\tForwarding Discriminator")
                     res, label = self.D(x_fake)
 
-                    g_real_res = torch.zeros(batch_size).to(device)
-                    x_fake2 = self.G(x_fake, l)
+                    g_real_res = torch.zeros(batch_size).to(self.device)    
 
+                    print("\tGenerating Fake Image")
+                    x_fake2 = self.G(x_fake, l)
                     g_loss = self.generator_loss(g_real_res, res, random_l, label, x, x_fake2)
+                    print(f"\tG_loss:{g_loss}")
+
                     g_loss.backward()
                     g_optimizer.step()
 
